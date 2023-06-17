f31a40cdc5017e582b752322944e19aeba318e20
HaoHe <hh19980711@163.com>
2023-06-17 16:35:42 +0800

    add calculater test

---
+++ src/main/java/Calculater.java
@@ -0,0 +1,6 @@
+public class Calculater {
+    public int compute(int num1, int num2, char symbol) {
+
+        return -1;
+    }
+}
---
+++ src/test/java/CalculaterTest.java
@@ -0,0 +1,22 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculaterTest {
+    @Test
+    public void shouldReturn2When1Plus1(){
+        Calculater calculater = new Calculater();
+        int actualResult = calculater.compute(1, 1, '+');
+        int expectResult = 2;
+        Assert.assertEquals(expectResult, actualResult);
+    }
+
+    @Test
+    public void shouldReturn0When1Sub1(){
+        Calculater calculater = new Calculater();
+        int actualResult = calculater.compute(1, 1, '-');
+        int expectResult = 0;
+        Assert.assertEquals(expectResult, actualResult);
+    }
+}
