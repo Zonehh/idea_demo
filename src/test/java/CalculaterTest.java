6ee3f057a8f17c6a0612bda8deedf436152a4412
HaoHe <hh19980711@163.com>
2023-06-17 17:19:38 +0800

    refactor calculater to use reflect

--- src/test/java/CalculaterTest.java
+++ src/test/java/CalculaterTest.java
@@ -5,17 +5,17 @@ import static org.junit.Assert.*;
 
 public class CalculaterTest {
     @Test
-    public void shouldReturn2When1Plus1(){
+    public void shouldReturn2When1Plus1() throws ClassNotFoundException, InstantiationException, IllegalAccessException {
         Calculater calculater = new Calculater();
-        int actualResult = calculater.compute(1, 1, '+');
+        int actualResult = calculater.compute(1, 1, "Add");
         int expectResult = 2;
         Assert.assertEquals(expectResult, actualResult);
     }
 
     @Test
-    public void shouldReturn0When1Sub1(){
+    public void shouldReturn0When1Sub1() throws ClassNotFoundException, InstantiationException, IllegalAccessException {
         Calculater calculater = new Calculater();
-        int actualResult = calculater.compute(1, 1, '-');
+        int actualResult = calculater.compute(1, 1, "Sub");
         int expectResult = 0;
         Assert.assertEquals(expectResult, actualResult);
     }
